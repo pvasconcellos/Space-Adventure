c1842985aad73459a2bc46ca3ff7738760e2333b
gvasconcellos20 <gvasconcellos20@300-computer-lab-12.local>
2017-01-27 09:48:32 -0500

    Initial Commit

---
+++ Space Adventure/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Space Adventure
+//
+//  Created by gvasconcellos20 on 1/27/17.
+//  Copyright © 2017 gvasconcellos20. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
